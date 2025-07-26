88c32850a177dd40c01a690e60ab12b2da001154
Matthew Berhe <matthew.berhe1@gmail.com>
2025-07-25 21:27:39 -0400

    url bug

--- frontend/src/app/lobby/page.tsx
+++ frontend/src/app/lobby/page.tsx
@@ -8,7 +8,7 @@ export default function Lobby() {
   useEffect(() => {
     const testAPI = async () => {
       try {
-        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/");
+        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL ?? "");
         const msg = await response.text();
         // console.log(response);
         setMessage(msg);
