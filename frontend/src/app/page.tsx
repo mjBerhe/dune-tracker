950a144fe0b88775e3f83b4626f52c349b899858
Matthew Berhe <matthew.berhe1@gmail.com>
2025-07-27 23:21:25 -0400

    create game endpoint working locally

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Home() {
+  const handleClick = async () => {
+    const res = await fetch("http://localhost:8787/create-game", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ host: "Matt", maxPlayers: 4 }),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <main className="p-8">
-      <h1 className="text-2xl font-bold">Multiplayer Game</h1>
+      <h1 className="text-2xl font-bold">Arrakis Observer</h1>
       <div className="mt-4 space-x-4">
-        <Link href="/lobby" className="underline text-blue-500">
+        <button onClick={handleClick}>Create Game</button>
+        {/* <Link href="/lobby" className="underline text-blue-500">
           Join Lobby
         </Link>
         <Link href="/game/test-room" className="underline text-blue-500">
           Enter Room
-        </Link>
+        </Link> */}
       </div>
     </main>
   );
--- hono-backend/src/index.ts
+++ hono-backend/src/index.ts
@@ -1,18 +1,66 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { success, z } from "zod";
+
+const createGameSchema = z.object({
+  host: z.string().min(1), // ensures non-empty stringf
+  maxPlayers: z.number(),
+});
+
+type CreateGameInput = z.infer<typeof createGameSchema>;
+
+type GameRoom = {
+  id: string;
+  name: string;
+  maxPlayers: number;
+  players: { id: string; name: string; money: number }[];
+  started: boolean;
+  // createdAt?
+};
+
+const rooms = new Map<string, GameRoom>();
+
+// Generate simple room ID
+function generateRoomId() {
+  return Math.random().toString(36).slice(2, 8).toUpperCase();
+}
 
 const app = new Hono();
 
 app.use(
   "*",
   cors({
-    origin: ["http://localhost:3000", "https://arrakis-observer.vercel.app/"], // Allow your local Next.js frontend
+    origin: ["http://localhost:3000", "https://arrakis-observer.vercel.app"], // Allow your local Next.js frontend
     credentials: true,
   })
 );
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.post("/create-game", async (c) => {
+  const body = await c.req.json();
+
+  const parsed = createGameSchema.safeParse(body);
+  if (!parsed.success) {
+    return c.json({ success: false, error: parsed.error }, 400);
+  }
+
+  const { host, maxPlayers } = parsed.data;
+  const id = generateRoomId();
+
+  const newGame: GameRoom = {
+    id,
+    name: host,
+    maxPlayers,
+    players: [{ id: "0", name: host, money: 0 }],
+    started: false,
+  };
+
+  rooms.set(id, newGame);
+
+  return c.json({ success: true, game: newGame });
 });
 
 export default app;
